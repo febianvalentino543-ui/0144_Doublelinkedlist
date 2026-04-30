553c5a9629dc18c304b98824cfe63d8029d71254
febianvalentino543-ui <febianvalentino543@gmail.com>
2026-04-30 13:49:51 +0700

    menambahkan addNode ditengah

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -55,3 +55,21 @@ public:
             return;
         }
 
+        // Insert in between node
+        // Step 8: locate position for insert
+        Node *current = START;
+        while (current->next != NULL && current->next->noMhs < nim)
+        {
+            current = current->next;
+        }
+
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll numbers not allowed" << endl;
+            return;
+        }
+
+        // Step 9: Insert newNode between current and current->next
+        newNode->next = current->next; // Step 9a: newNode->next = current->next
+        newNode->prev = current;       // Step 9b: newNode->prev = current
+
