68cffe91973fbe94238f0e27cff4c713374bf3e1
febianvalentino543-ui <febianvalentino543@gmail.com>
2026-04-30 13:50:32 +0700

    menambahkan addNote diakhir

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -73,3 +73,10 @@ public:
         newNode->next = current->next; // Step 9a: newNode->next = current->next
         newNode->prev = current;       // Step 9b: newNode->prev = current
 
+        // Insert last node
+        if (current->next != NULL) 
+            current->next->prev = newNode; // Step 9c: current.next.prev = newNode
+
+        current->next = newNode; // Step 9d: current.next = newNode
+    }
+
