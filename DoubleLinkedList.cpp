c1bba837cf061d87e4d3a2e885197b5d3ec7bb70
febianvalentino543-ui <febianvalentino543@gmail.com>
2026-04-30 13:57:16 +0700

    menambahkan while dalam traverse

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -141,3 +141,13 @@ public:
         cout << "\nRecords in ascending order of roll number are:\n";
         int i = 0;
 
+        while (currentNode != NULL)
+        {
+            cout << i + 1 << ". " << currentNode->noMhs << endl;
+
+            // Step 3: move to next node
+            currentNode = currentNode->next;
+            i++;
+        }
+    }
+
